f84f9b19f7abd5f1794d9e610ecb2d2540c09db2
Michael Steck <steck@anabrid.com>
2023-03-10 16:48:08 +0100

    WIP protocol: SuccessInfo docstring

--- pyanabrid/redac/protocol/types.py
+++ pyanabrid/redac/protocol/types.py
@@ -29,5 +29,10 @@ from pydantic import BaseModel
 
 
 class SuccessInfo(BaseModel):
+    """
+    Data type used in responses indicating whether the associated request was successful.
+    """
+    #: :code:`True` if response was successful, :code:`False` otherwise.
     success: bool
+    #: An optional error message. Always None if success is :code:`True`.
     error: typing.Optional[str]
