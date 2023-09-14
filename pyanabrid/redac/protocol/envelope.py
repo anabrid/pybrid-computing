9b2dbc7f8ba08caa3759dfec5f3b6a2293ee97c8
Michael Steck <steck@anabrid.com>
2023-09-14 13:31:19 +0200

    doc: docstring for Envelope class

--- pyanabrid/redac/protocol/envelope.py
+++ pyanabrid/redac/protocol/envelope.py
@@ -41,10 +41,32 @@ class MalformedEnvelopeError(MalformedDataError):
 
 
 class Envelope(BaseModel):
+    """
+    Envelope containing a :class:`Message`.
+
+    While the relevant data for the communication as part of this protocol is encoded
+    using the various message classes, the most fundamental data package that is sent
+    between two recipients is this envelope.
+
+    The envelope contains an :attr:`id`, which is used to identify responses to previous requests
+    (optional for notifications).
+
+    The :attr:`type` field contains a unique type identifier, telling the recipient which :class:`Message`
+    is contained in the :attr:`msg` field. The type identifier is defined to be the python message class name,
+    with any Request, Response or Notification suffix removed and converted to underscore case.
+
+    For responses, the :attr:`success` field and :attr:`error` field define whether the request was successfully
+    handled. Only if :attr:`success` is true, :attr:`msg` contains an actual response.
+    """
+    #: Optional ID of the request and its response. None for Notifications
     id: typing.Optional[UUID] = Field(default_factory=uuid4)
+    #: Unique string defining the type of the contained message
     type: str
+    #: The msg, None if :attr:`success` is false for responses
     msg: typing.Optional[dict] = None
+    #: Whether the request was handled successfully (only in responses)
     success: typing.Optional[bool] = Field(exclude=True, default=True)
+    #: Optional error, in case :attr:`success` is false
     error: typing.Optional[str] = Field(exclude=True, default="")
 
     @classmethod
