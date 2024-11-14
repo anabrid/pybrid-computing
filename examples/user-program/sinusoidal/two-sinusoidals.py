ebebe2227a3765cf927af4c3f2f91bd6277e0c9b
Michael Steck <steck@anabrid.com>
2024-11-14 15:33:17 +0100

    examples: fix single sinusoidal and add one for two sinusoidals on two carrier boards

---
+++ examples/user-program/sinusoidal/two-sinusoidals.py
@@ -0,0 +1,35 @@
+# Example of a UserProgram configuring a harmonic oscillator on the REDAC. Use as
+#   pybrid redac -h <host> user-program path/to/this/file.py
+
+import matplotlib.pyplot as plt
+
+from pybrid.base.hybrid.programs import SimpleRun
+from pybrid.redac import REDAC, Run, RunConfig, DAQConfig
+
+
+class UserProgram(SimpleRun):
+    # Shortcut to configure run
+    RUN_CONFIG = RunConfig(op_time=2_560_000)
+    DAQ_CONFIG = DAQConfig(num_channels=2, sample_rate=100_000)
+
+    def set_configuration(self, run: Run, computer: REDAC):
+        for carrier in computer.carriers:
+            for cluster in carrier.clusters:
+                # Configure harmonic oscillator
+                cluster.route(0, 0, -1.0, 1)
+                cluster.route(1, 1, 1.0, 0)
+                # Configure initial value
+                cluster.m0block.elements[0].ic = 0.42
+
+                # Configure which signals you want to capture
+                computer.daq.capture(cluster.m0block.elements[0], cluster.m0block.elements[1])
+
+    def run_done(self, run: Run):
+        # This function is called once the run is done
+        if run.data:
+            for channel in run.data.values():
+                plt.plot(channel)
+            plt.ylabel("Amplitude x")
+            plt.xlabel("'Time' t")
+            plt.show()
+        self.print("Done.")
