6a6022ca86da8e5ab29f4fd162f014c1e71a5b83
Michael Steck <steck@anabrid.com>
2024-11-21 08:46:58 +0100

    feat(controller): possibility to use usb-spi adapter to send SYNC signals to start a run

---
+++ pybrid/redac/sync.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2022-2024 anabrid GmbH
+# Contact: https://www.anabrid.com/licensing/
+# SPDX-License-Identifier: MIT OR GPL-2.0-or-later
+
+import time
+
+from pyftdi.spi import SpiController
+
+
+class Sync:
+
+    def __init__(self):
+        self.spi = SpiController()
+        self.spi.configure("ftdi://ftdi:232h:1/1")
+        self.mosi = self.spi.get_port(cs=0, freq=12e6, mode=0)
+
+    def trigger(self):
+        # TODO: Allow triggering different SYNC_ID
+        self.mosi.write([0b11110000, 0b10101010])
