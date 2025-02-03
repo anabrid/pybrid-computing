66a3639c7554122d3033fd2230e47706c98afa70
Michael Steck <steck@anabrid.com>
2025-02-03 21:28:41 +0100

    WIP proxy: Change to using a Protocol instance to handle incoming messages, initial implementation for forwarding SetCircuitRequests and handling StartRunRequests

--- pybrid/redac/proxy.py
+++ pybrid/redac/proxy.py
@@ -3,23 +3,19 @@
 # SPDX-License-Identifier: MIT OR GPL-2.0-or-later
 
 import asyncio
-import json
 import logging
 from asyncio import StreamReader, StreamWriter, Server
 from typing import Optional
 
 from pybrid.base.hybrid import EntityDoesNotExist
-from pybrid.redac import Controller, Run, Path
-from pybrid.redac.protocol.envelope import Envelope
+from pybrid.base.transport import PassthroughTransport
+from pybrid.redac import Controller, Path, Protocol
 from pybrid.redac.protocol.messages import (
     SetCircuitRequest,
-    ResumeSessionRequest,
     StartRunRequest,
-    ResumeSessionResponse,
     SetCircuitResponse,
     StartRunResponse,
 )
-from pybrid.redac.protocol.types import SuccessInfo
 
 logger = logging.getLogger(__name__)
 
@@ -53,40 +49,17 @@ class Proxy:
         peer = writer.get_extra_info("peername")
         logger.debug("Established incoming connection from %s.", peer)
 
-        async with asyncio.timeout(10):
-            envelope_in = Envelope(**json.loads((await reader.readline()).decode().strip()))
-            resume_session = envelope_in.get_message(msg_class=ResumeSessionRequest)
-            logger.debug("Received ResumeSessionRequest: %s", resume_session)
-            envelope_out = Envelope.from_message(
-                ResumeSessionResponse(success=SuccessInfo(success=True)), id_=envelope_in.id
-            )
-            writer.write(envelope_out.json().encode())
-            writer.write(b"\n")
-            await writer.drain()
+        # Initiate a subordinate protocol that defaults to treating incoming messages as requests
+        transport = await PassthroughTransport.create(reader, writer, name=str(peer))
+        protocol = await Protocol.create(transport)
+        # Register callbacks
+        protocol.register_callback(SetCircuitRequest, self.handle_set_circuit, extra_args=[protocol])
+        protocol.register_callback(StartRunRequest, self.handle_start_run, extra_args=[protocol])
 
-        async with asyncio.timeout(10):
-            envelope_in = Envelope(**json.loads((await reader.readline()).decode().strip()))
-            set_circuit = envelope_in.get_message(msg_class=SetCircuitRequest)
-            logger.debug("Received SetCircuitRequest: %s", set_circuit)
-            logger.debug("Forwarding SetCircuitRequest...")
-            await (await self.controller.forward_set_circuit(set_circuit))
-            envelope_out = Envelope.from_message(SetCircuitResponse(), id_=envelope_in.id)
-            writer.write(envelope_out.json().encode())
-            writer.write(b"\n")
-            await writer.drain()
-
-        async with asyncio.timeout(10):
-            envelope_in = Envelope(**json.loads((await reader.readline()).decode().strip()))
-            start_run = envelope_in.get_message(msg_class=StartRunRequest)
-            logger.debug("Received StartRunRequest: %s", start_run)
-            logger.debug("Forwarding StartRunRequest...")
-            run = Run(id_=start_run.id, config=start_run.config, daq=start_run.daq_config)
-            await self.controller.start_and_await_run(run)
-            logger.debug("Run is done.")
-            envelope_out = Envelope.from_message(StartRunResponse(), id_=envelope_in.id)
-            writer.write(envelope_out.json().encode())
-            writer.write(b"\n")
-            await writer.drain()
+        # Start protocol and let it process incoming messages
+        async with protocol:
+            logger.debug("Initiated protocol communication with %s. Waiting for incoming requests...", peer)
+            await protocol
 
         writer.close()
         await writer.wait_closed()
@@ -98,3 +71,30 @@ class Proxy:
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self._server.__aexit__(exc_type, exc_val, exc_tb)
+
+    # ██   ██  █████  ███    ██ ██████  ██      ███████ ██████  ███████
+    # ██   ██ ██   ██ ████   ██ ██   ██ ██      ██      ██   ██ ██
+    # ███████ ███████ ██ ██  ██ ██   ██ ██      █████   ██████  ███████
+    # ██   ██ ██   ██ ██  ██ ██ ██   ██ ██      ██      ██   ██      ██
+    # ██   ██ ██   ██ ██   ████ ██████  ███████ ███████ ██   ██ ███████
+
+    async def handle_set_circuit(self, msg: SetCircuitRequest, protocol: Protocol):
+        logger.debug("Handling %s from %s", type(msg), protocol.transport.name)
+
+        # Re-map carrier
+        mapped_config = dict()
+        for original_carrier_id, carrier_config in msg.config.items():
+            mapped_config[self.mac_mapping[original_carrier_id]] = carrier_config
+        # Forward
+        msg.config = mapped_config
+        await self.controller.forward_set_circuit(msg)
+
+        return SetCircuitResponse()
+
+    async def handle_start_run(self, msg: StartRunRequest, protocol: Protocol):
+        logger.debug("Handling %s from %s", type(msg), protocol.transport.name)
+
+        run = msg.to_run()
+        await self.controller.start_and_await_run(run)
+
+        return StartRunResponse()
